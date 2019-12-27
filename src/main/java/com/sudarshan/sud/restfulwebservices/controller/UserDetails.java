38d8fdab03eb1e57a065153ec48204e522a28992
SudarshanKumar <sudarshan4309@gmail.com>
2019-12-27 11:45:42 +0530

    A controller for a user Model with a getOne and getAll method

---
+++ src/main/java/com/sudarshan/sud/restfulwebservices/controller/UserDetails.java
@@ -0,0 +1,29 @@
+package com.sudarshan.sud.restfulwebservices.controller;
+
+
+import com.sudarshan.sud.restfulwebservices.model.User;
+import com.sudarshan.sud.restfulwebservices.service.UserDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserDetails {
+
+    @Autowired
+    private UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+}
---
+++ src/test/java/com/sudarshan/sud/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudarshan.sud.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

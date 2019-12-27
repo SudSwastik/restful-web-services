c2dafd064ae6a102d38fa01bd67a01ef4497573d
SudarshanKumar <sudarshan4309@gmail.com>
2019-12-27 20:33:02 +0530

    Added Post Mapping For Adding User

--- src/main/java/com/sudarshan/sud/restfulwebservices/controller/UserDetails.java
+++ src/main/java/com/sudarshan/sud/restfulwebservices/controller/UserDetails.java
@@ -4,9 +4,7 @@ package com.sudarshan.sud.restfulwebservices.controller;
 import com.sudarshan.sud.restfulwebservices.model.User;
 import com.sudarshan.sud.restfulwebservices.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +22,11 @@ public class UserDetails {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
     }
 
 }

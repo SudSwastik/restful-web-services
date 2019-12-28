9d983ec8e90dc3c4d8073b8207fa73bb74a6c87c
SudarshanKumar <sudarshan4309@gmail.com>
2019-12-28 18:50:26 +0530

    Created Delete Mapping

--- src/main/java/com/sudarshan/sud/restfulwebservices/controller/UserDetails.java
+++ src/main/java/com/sudarshan/sud/restfulwebservices/controller/UserDetails.java
@@ -43,4 +43,12 @@ public class UserDetails {
 
         return ResponseEntity.created(location).build();
     }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if(user==null)
+            throw new UserNotFoundException("id-"+ id);
+    }
 }
